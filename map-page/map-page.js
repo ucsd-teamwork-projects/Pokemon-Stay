c0e9c6fff7828dc87b533e125e0dc5b672d30708
edelBCS <edelm8@gmail.com>
2019-08-03 13:36:26 -0700

    added map-page with searchable map

--- map-page/map-page.js
+++ map-page/map-page.js
@@ -1,58 +1,66 @@
-var mapOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng()
-};
-var map = new google.maps.Map(
-    document.getElementById(
-        'map-container'),
-    mapOptions);
-//the first marker, with everything expanded for clarity
-var houseIcon = {
-    /*
-     Position an image to appear at the lat/long point
-     For this image, the marker point is in the middle, at the bottom
-    */
-    anchor: new google.maps.Point(16, 37),
-    /*
-     Position of the image within the sprite
-     In  this sprite,  the images are arranged horizontally
-     so 0,0 is the start of the first image and 111,0 is the 3rd image
-    */
-    origin: new google.maps.Point(111, 0),
-    /*Size of the sprite image*/
-    size: new google.maps.Size(32, 37),
-    /*url of the sprite image*/
-    url: '.....'
-};
-var houseMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(51.502066, -0.140122),
-    map: map,
-    icon: houseIcon
-});
-/*and the rest of the markers (more efficiently coded)*/
-var iconAnchor = new google.maps.Point(16, 37);
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap'
+    });
 
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
 
-var iconSize = new google.maps.Size(32, 37);
-var iconUrl = '......';
-var birthMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(fill in , fill in ),
-    map: map,
-    icon: {
-        anchor: iconAnchor,
-        origin: new google.maps.Point(74, 0),
-        size: iconSize,
-        url: iconUrl
-    }
-});
-var workMarker = new google.maps.Marker({
-    position: new google.maps.LatLng(fill in , fill in ),
-    map: map,
-    icon: {
-        anchor: iconAnchor,
-        origin: new google.maps.Point(0, 0),
-        size: iconSize,
-        url: iconUrl
-    }
-});
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
